074dc869abe4d7f964104a530ad353b4b0f66e85
juancho <jm.ticona.p@gmail.com>
2023-01-25 11:22:17 -0500

    First config for dockerize reservas-api

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require('sequelize')
-process.env.NODE_ENV === 'dev' && require('dotenv').config()
+require('dotenv').config()
 
 const db = new Sequelize(
   process.env.NODE_ENV === 'pro' ? process.env.PRO_DB_URI : process.env.DEV_DB_URI,
   {
     logging: false,
-    dialectOptions: process.env.NODE_ENV === 'pro' && { ssl: { require: true, rejectUnauthorized: false } }
+    //dialectOptions: process.env.NODE_ENV === 'pro' && { ssl: { require: true, rejectUnauthorized: false } }
   }
 )
 
