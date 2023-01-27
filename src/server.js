039945f59a465b7d31160e69e474cfadb2f7267f
juancho <jm.ticona.p@gmail.com>
2023-01-27 11:24:31 -0500

    FIX env PORT

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const app = require('./app')
 
 const HOSTNAME = 'localhost'
-const PORT = '8000'
+const PORT = process.env.PORT
+  ? process.env.PORT
+  : '8000'
 
 const server = app.listen(PORT, HOSTNAME, () => {
   console.log(`Server running\nhttp://${HOSTNAME}:${PORT}`)
